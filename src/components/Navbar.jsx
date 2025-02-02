c026f1bc4eab6c03a9e498a62b7dc51e51cc3cd1
Rishika Agarwal <rishikaagarwal2316@gmail.com>
2025-02-02 23:02:53 +0530

    animation

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-zinc-950 w-[110vw] ml-[-100px]'>
+       <p className='text-white'></p>
+    </div>
+  )
+}
+
+export default Navbar
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,28 @@ export default {
   theme: {
     extend: {},
   },
+  colors: {
+    dark: {
+      100: "#0D0D0D",
+      200: "#121212",
+      300: "#181818",
+      400: "#1E1E1E",
+      500: "#252525",
+      600: "#2D2D2D",
+    },
+    accent: {
+      orange: "#FF4C29",
+      pink: "#FF3860",
+      purple: "#8B5CF6",
+      cyan: "#06B6D4",
+      aqua: "#14FFEC",
+      yellow: "#FACC15",
+      crimson: "#E63946",
+      copper: "#9A3412",
+      emerald: "#36D399",
+      indigo: "#6366F1",
+      rose: "#F43F5E",
+    },
+  },
   plugins: [],
 }
