2350209f1666a1feeea05751eb73837e1417c55d
Rishika Agarwal <rishikaagarwal2316@gmail.com>
2025-02-03 23:30:29 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,44 @@
-import './App.css'
-import Navbar from './components/Navbar';
-import Squares from './components/Squares';
-
+import "./App.css";
+import Navbar from "./components/Navbar";
+import SplitText from "./components/SplitText";
+import Squares from "./components/Squares";
 
 function App() {
+  return (
+    <div className="relative">
+   <div className="absolute mt-[-270px]"> 
+   <Navbar />
+   </div>
 
-return (
-<>
-<Navbar />
+      <div className="bg-zinc-950 flex justify-center items-center">
+     <div className="absolute">
+     <Squares
+          speed={0.2}
+          squareSize={38}
+          direction="diagonal" // up, down, left, right, diagonal
+          borderColor="#222222"
+          hoverFillColor="#222"
+        />
+     </div>
 
-  
-<Squares 
-speed={0.2} 
-squareSize={38}
-direction='diagonal' // up, down, left, right, diagonal
-borderColor='#fff'
-hoverFillColor='#222'
-/>
-</>
+     <div>
+     <SplitText
+          text="Hello, I am Rishika!"
+          className="text-5xl font-bold text-center"
+          delay={40}
+          animationFrom={{ opacity: 0, transform: "translate3d(0,50px,0)" }}
+          animationTo={{ opacity: 1, transform: "translate3d(0,0,0)" }}
+          easing="easeOutCubic"
+          threshold={0.2}
+          rootMargin="-50px"
+        />
+     </div>
 
- );
+
+
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='bg-zinc-950 w-[110vw] ml-[-100px]'>
-       <p className='text-white'></p>
+    <div className='bg-zinc-950 w-[110vw] ml-[-100px] absolute'>
+       <p className='text-white'>Hi am navbar</p>
     </div>
   )
 }
